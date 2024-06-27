b49480f413d2b9b538eae413e7334ca16476594b
ceccobell <francescobell2001@gmail.com>
2024-06-27 15:15:31 +0200

    [UPD] exercise complete with bonus

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
+const container = document.getElementById("container");
+
 for (let i = 1; i <= 100; i++) {
+  let square = document.createElement("div");
+  square.classList.add("square");
+  container.append(square);
+  let content = document.createElement("span");
+  square.append(content);
   if (i % 3 == 0 && i % 5 == 0) {
-    console.log("FizzBuzz");
+    content.textContent = "FizzBuzz";
+    square.classList.add("pink");
   } else if (i % 5 == 0) {
-    console.log("Buzz");
+    content.textContent = "Buzz";
+    square.classList.add("yellow");
   } else if (i % 3 == 0) {
-    console.log("Fizz");
+    content.textContent = "Fizz";
+    square.classList.add("green");
   } else {
-    console.log(i);
+    content.textContent = i;
+    square.classList.add("azure");
   }
 }
